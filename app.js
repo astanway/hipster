04b49304bf448a09d37cd07f5d2e9760413a3168
Abe Stanway <abestanway@gmail.com>
2012-04-22 03:04:46 -0400

    Left and right

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express')
+  var express = require('express')
 var app = express.createServer()
 var static = require('node-static');
 var jquery = require('jquery');
@@ -10,7 +10,11 @@ app.use(express.static(__dirname + "/js"));
 
 var root = ''
 
-app.get('/', function (req, res) {
+app.get('/stageleft', function (req, res) {
+  res.sendfile(__dirname + '/phone.html');
+});
+
+app.get('/stageright', function (req, res) {
   res.sendfile(__dirname + '/phone.html');
 });
 
@@ -23,20 +27,27 @@ io.sockets.on('connection', function (socket) {
     if(data.b > 0 && Math.abs(data.g) < 70){
       angle = data.b
       console.log("upward strike " + angle)
+      console.log(data.player);
     }
     
     if(data.b > 0 && Math.abs(data.g) >= 70){
       angle = 180 - data.b
       console.log("downward strike " + angle)
+      console.log(data.player);
     }
     
     if(data.b < 0){
       angle = 180 - data.b
       console.log("underhand strike " + angle);
+      console.log(data.player);
     }
   })
 
   socket.on('wantVelocity', function(data){
-    socket.emit('getVelocity')
+    if(data == 1){
+      socket.broadcast.emit('getstageleftVelocity')
+    } else {
+      socket.broadcast.emit('getstagerightVelocity') 
+    }
   })
 });
